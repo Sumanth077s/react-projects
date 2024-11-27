85d1d291c335ea89d480d1a9c8439709f241f9e2
Sumanth U <157620444+Sumanth077s@users.noreply.github.com>
2024-11-27 15:14:00 +0530

    Update Tours.js

--- 02-tours/final/src/Tours.js
+++ 02-tours/final/src/Tours.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import Tour from './Tour';
+import React from 'react'; // Importing React library
+import Tour from './Tour'; // Importing the Tour component
+
+// Tours component receives a list of tours and a function to remove a tour as props
 const Tours = ({ tours, removeTour }) => {
   return (
     <section>
+      {/* Section header */}
       <div className="title">
         <h2>our tours</h2>
-        <div className="underline"></div>
+        <div className="underline"></div> {/* Decorative underline */}
       </div>
       <div>
+        {/* Iterating through the tours array and rendering a Tour component for each tour */}
         {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+          return (
+            <Tour 
+              key={tour.id} // Unique key for each tour
+              {...tour} // Spread operator to pass all tour properties as props
+              removeTour={removeTour} // Passing the removeTour function as a prop
+            />
+          );
         })}
       </div>
     </section>
   );
 };
 
-export default Tours;
+export default Tours; // Exporting the Tours component
