f40c34f3a365da672568c8ba39b25b5bd7c791ae
Sumanth U <157620444+Sumanth077s@users.noreply.github.com>
2024-12-01 05:54:38 +0530

    Update App.js

--- 04-accordion/final/src/App.js
+++ 04-accordion/final/src/App.js
@@ -1,15 +1,28 @@
+// Importing React and useState for component creation and state management
 import React, { useState } from 'react';
+
+// Importing the data array from an external file
 import data from './data';
+
+// Importing the SingleQuestion component to display individual questions
 import SingleQuestion from './Question';
+
 function App() {
+  // Initializing state to hold the questions data
   const [questions, setQuestions] = useState(data);
+
   return (
     <main>
       <div className='container'>
+        {/* Page heading */}
         <h3>questions and answers about login</h3>
+
+        {/* Section to display the list of questions */}
         <section className='info'>
+          {/* Mapping through the questions array to render each question */}
           {questions.map((question) => {
             return (
+              // Rendering SingleQuestion component for each question with props spread
               <SingleQuestion key={question.id} {...question}></SingleQuestion>
             );
           })}
@@ -19,4 +32,5 @@ function App() {
   );
 }
 
+// Exporting the App component as the default export
 export default App;
