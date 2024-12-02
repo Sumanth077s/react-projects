28afa0fbe6f036c2ca5516c6c39f34847e5125f5
Sumanth U <157620444+Sumanth077s@users.noreply.github.com>
2024-12-02 16:55:51 +0530

    Update Loading.js

--- 23-quiz/setup/src/Loading.js
+++ 23-quiz/setup/src/Loading.js
@@ -1,11 +1,16 @@
+// Importing the React library to create and manage the component
 import React from 'react'
 
+// Defining the Loading functional component
 const Loading = () => {
   return (
+    // Main container for the loading component
     <main>
+      {/* Div with a className 'loading', typically styled to display a loading animation */}
       <div className='loading'></div>
     </main>
   )
 }
 
+// Exporting the Loading component for use in other parts of the application
 export default Loading
