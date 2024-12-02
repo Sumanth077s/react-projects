adf751287178cae24a392b88c479ad800aed24d0
Sumanth U <157620444+Sumanth077s@users.noreply.github.com>
2024-12-03 05:01:04 +0530

    Update index.js

--- 23-quiz/final/src/index.js
+++ 23-quiz/final/src/index.js
@@ -1,15 +1,26 @@
+// Importing React library for creating components
 import React from 'react';
+
+// Importing ReactDOM to handle rendering React components to the DOM
 import ReactDOM from 'react-dom/client';
+
+// Importing the CSS file for styling the application
 import './index.css';
+
+// Importing the main App component
 import App from './App';
+
+// Importing AppProvider, which provides global context to the application
 import { AppProvider } from './context';
 
+// Creating a root DOM node where the React application will be rendered
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Rendering the React application to the root element
 root.render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
+  <React.StrictMode> {/* Enables additional checks and warnings in development mode */}
+    <AppProvider>    {/* Wrapping the App component with AppProvider to enable global state management */}
+      <App />         {/* Rendering the main App component */}
     </AppProvider>
   </React.StrictMode>
 );
