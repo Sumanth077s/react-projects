03bac8b0d400a5f27f9ce438326b2d33f09ce0ad
Sumanth U <157620444+Sumanth077s@users.noreply.github.com>
2024-12-07 07:32:28 +0530

    Update Stories.js

--- 22-hacker-news/setup/src/Stories.js
+++ 22-hacker-news/setup/src/Stories.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react' // Import React to create the functional component
 
+// Import the context hook from a custom context file
 import { useGlobalContext } from './context'
 
+// Define the Stories component
 const Stories = () => {
+  // Placeholder content to display while the component is being developed
   return <h2>stories component</h2>
 }
 
+// Export the Stories component as the default export for use in other files
 export default Stories
